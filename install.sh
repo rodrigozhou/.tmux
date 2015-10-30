9aab36feab06d5422ef359a8caf34ab6b9a7f41a
rodrigozhou <rodrigo.zhou@gmail.com>
2015-10-30 17:07:59 -0200

    removing theme

--- install.sh
+++ install.sh
@@ -7,6 +7,4 @@ pkg="reattach-to-user-namespace"
 brew list -1 | grep -q "^$pkg\$" || { brew install $pkg; }
 
 git clone https://github.com/rodrigozhou/.tmux.git $HOME/.tmux
-git clone https://github.com/seebi/tmux-colors-solarized.git \
-    $HOME/.tmux/tmux-colors-solarized
 ln -s $HOME/.tmux/tmux.conf $HOME/.tmux.conf
